f19455a92f8e42d6794bb965962dc88b546df5d2
Zeu Capua <coding@zeu.dev>
2025-01-21 19:12:03 -0800

    refactor user store, comment out clientMetadata, init client load function

--- src/lib/atproto.ts
+++ src/lib/atproto.ts
@@ -1,21 +1,23 @@
-import { BrowserOAuthClient } from "@atproto/oauth-client-browser";
 import { dev } from "$app/environment";
+import { BrowserOAuthClient } from "@atproto/oauth-client-browser";
 
 // TODO: change to permanent public URL
 const publicUrl = "https://pigeon.muni.town"
 // localhost resolves to either 127.0.0.1 or [::1] (if ipv6)
 const url = dev ? "http://[::1]:5173" : publicUrl;
 
+const clientId = !dev ? `${publicUrl}/client-metadata.json`
+  : `http://localhost?redirect_uri=${
+    encodeURIComponent(`${url}/oauth/callback`)
+  }&scope=${
+    encodeURIComponent(`atproto transition:generic`)
+  }`;
+
+/**
 export const clientMetadata = {
   client_name: "Pigeon",
-  application_type: "web",
-  client_id: !dev ? `${publicUrl}/client-metadata.json`
-    : `http://localhost?redirect_uri=${
-      encodeURIComponent(`${url}/oauth/callback`)
-    }&scope=${
-      encodeURIComponent(`atproto transition:generic`)
-    }`,
   client_uri: url,
+  application_type: "web",
   redirect_uris: [`${url}/oauth/callback`], 
   scope: "atproto transition:generic",
   grant_types: ["authorization_code", "refresh_token"],
@@ -23,9 +25,17 @@ export const clientMetadata = {
   dpop_bound_access_tokens: true,
   response_types: ["code"]
 };
+**/
 
-export const atprotoClient = new BrowserOAuthClient({
-  clientMetadata,
-  handleResolver: 'https://public.api.bsky.app'
-} as any);
 
+export async function createAtProtoClient() {
+  const client = BrowserOAuthClient.load({
+    clientId,
+    handleResolver,
+    responseMode,
+    plcDirectoryUrl,
+    fetch,
+    allowHttp,
+    signal,
+  });
+}
--- src/lib/user.svelte.ts
+++ src/lib/user.svelte.ts
@@ -1,20 +1,9 @@
-import { atprotoClient } from "./atproto";
+function createUserStore() {
+  let state = $state();
+  let session = $state();
 
-export class UserStore {
-  #state = $state();
-  #session = $state();
-
-  constructor() {}
-
-  public static async build(): Promise<UserStore> {
-    const result = await atprotoClient.init();
-    if (result) {
-      const { session, state } = result;
-      if (state !== null) {
-        this.#state = state;
-        this.#session = session;
-      }
-    }
-    return new UserStore() 
+  return {
+    get state() { return state },
+    get session() { return session }
   }
 }
