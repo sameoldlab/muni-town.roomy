4679a67ab6a58f75a1544969084276112acb7a20
Zeu Capua <coding@zeu.dev>
2025-02-01 02:47:02 -0800

    implement dm header tabs (z not working)

--- src/routes/(app)/dm/[did]/+page.svelte
+++ src/routes/(app)/dm/[did]/+page.svelte
@@ -6,10 +6,18 @@
   import { page } from "$app/state";
   import { user } from "$lib/user.svelte";
   import { onDestroy, setContext } from "svelte";
-  import { Avatar, Button } from "bits-ui";
+  import { Avatar, Button, Tabs } from "bits-ui";
   import { AvatarBeam } from "svelte-boring-avatars";
   import Icon from "@iconify/svelte";
+  import { crossfade } from "svelte/transition";
+  import { cubicInOut } from "svelte/easing";
 
+  const [send, receive] = crossfade({
+    duration: 150,
+    easing: cubicInOut
+  });
+
+  let tab = $state("chat");
   let channel: Autodoc<Channel> | undefined = $derived(g.dms[page.params.did]);
   let messageInput = $state("");
   let info = $derived(g.catalog?.view.dms[page.params.did]);
@@ -52,7 +60,7 @@
   });
 </script>
 
-<section class="flex flex-none items-center justify-between border-b-1 pb-4">
+<header class="flex flex-none items-center justify-between border-b-1 pb-4">
   <div class="flex gap-4 items-center">
     <Avatar.Root class="w-8">
       <Avatar.Image src={info?.avatar} class="rounded-full" />
@@ -65,22 +73,57 @@
     </h4>
   </div>
 
-  <menu>
+  <Tabs.Root bind:value={tab}>
+    <Tabs.List class="grid grid-cols-2 gap-4 border text-white p-1 rounded">
+      <Tabs.Trigger value="chat" class="relative group">
+        <span class="flex z-50 gap-2 w-full justify-center items-center px-4 py-1">
+          <Icon icon="tabler:message" color="white"  class="text-2xl" />
+          <p>Chat</p>
+        </span>
+        {#if tab === "chat"}
+					<div
+						class="absolute z-10 left-0 top-0 h-8 w-full rounded bg-violet-800"
+						in:send={{ key: "active" }}
+						out:receive={{ key: "active" }}
+					></div>
+				{/if}
+      </Tabs.Trigger>
+      <Tabs.Trigger value="threads" class="relative flex gap-2 w-full justify-center items-center px-4 py-1">
+        <Icon icon="material-symbols:thread-unread-rounded" color="white"  class="text-2xl" />
+        Threads
+        {#if tab === "threads"}
+					<div
+						class="absolute left-0 top-0 h-8 w-full rounded bg-violet-800"
+						in:send={{ key: "active" }}
+						out:receive={{ key: "active" }}
+					></div>
+				{/if}
+      </Tabs.Trigger>
+    </Tabs.List>
+  </Tabs.Root>
+
+  <menu class="flex items-center">
     <Button.Root class="hover:scale-105 active:scale-95 transition-all duration-150">
-      <Icon icon="tabler:message-cog" color="white"  class="text-2xl" />
+      <Icon icon="basil:settings-alt-solid" color="white" class="text-2xl" />
     </Button.Root>
   </menu>
-</section>
+</header>
 
-{#if channel}
-  <ChatArea {channel} />
+{#if tab === "chat"}
+  {#if channel}
+    <ChatArea {channel} />
+    <form onsubmit={sendMessage}>
+      <input
+        type="text"
+        class="w-full px-4 py-2 rounded-lg bg-violet-900 flex-none text-white"
+        placeholder="Say something..."
+        bind:value={messageInput}
+      />
+    </form>
+  {/if}
 {/if}
 
-<form onsubmit={sendMessage}>
-  <input
-    type="text"
-    class="w-full px-4 py-2 rounded-lg bg-violet-900 flex-none text-white"
-    placeholder="Say something..."
-    bind:value={messageInput}
-  />
-</form>
+<!-- TODO: Render Threads -->
+{#if tab === "threads"}
+  <p>Threads</p>
+{/if}
