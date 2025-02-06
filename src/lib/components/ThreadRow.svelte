d47865ad2ec9f20d70f3c2c1b32a7d932fca784e
Zeu Capua <coding@zeu.dev>
2025-02-05 16:16:42 -0800

    add latest message info to ThreadRow

--- src/lib/components/ThreadRow.svelte
+++ src/lib/components/ThreadRow.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
   import type { Ulid, Thread } from "$lib/schemas/types";
+  import { decodeTime } from "ulidx";
+  import { formatDistanceToNowStrict } from "date-fns";
 
   let { id, thread, onclick }: { id: Ulid, thread: Thread, onclick: () => void } = $props();
+  let latest = $derived.by(() => {
+    let allTime: number[] = [];
+    for (const id of thread.timeline) {
+      allTime.push(decodeTime(id));
+    }
+    allTime.sort();
+    return new Date(allTime[allTime.length-1]);
+  });
+
+  $inspect({ latest })
 </script>
 
-<button {id} {onclick} class="cursor-pointer text-white flex w-full justify-between p-4 hover:bg-white/5 transition-all duration-75">
-  <h1 class="text-xl font-semibold">{thread.title}</h1>
+<button {id} {onclick} class="border border-white rounded cursor-pointer text-white flex w-full justify-between p-4 hover:bg-white/5 transition-all duration-75">
+  <div class="flex gap-4 items-center">
+    <h1 class="text-xl font-semibold">{thread.title}</h1>
+    <time class="text-sm text-gray-300">Latest: {formatDistanceToNowStrict(latest)} ago</time>
+  </div>
   <p class="flex gap-2 items-center">
     {thread.timeline.length}
     <Icon icon="tabler:message" />
