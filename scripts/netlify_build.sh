83ac07ce160f8d4a473d8459e1c998fb49f39a75
Zicklag <zicklag@katharostech.com>
2025-03-22 21:40:06 -0500

    ci: fix Netlify production and pull request builds. (#106)

---
+++ scripts/netlify_build.sh
@@ -0,0 +1,32 @@
+#!/bin/env bash
+
+set -ex
+
+# Install dependencies
+deno upgrade
+deno install
+deno run build
+
+target_url=""
+if [ "$PULL_REQUEST" = "true" ]; then
+  target_url="$DEPLOY_URL"
+else 
+  target_url="$URL"
+fi
+
+# Add oauth-client configuration
+echo "{
+  \"client_id\": \"$target_url/oauth-client.json\",
+  \"client_name\": \"Roomy\",
+  \"client_uri\": \"$target_url\",
+  \"logo_uri\": \"$target_url/favicon.png\",
+  \"tos_uri\": \"$target_url\",
+  \"policy_uri\": \"$target_url\",
+  \"redirect_uris\": [\"$target_url/oauth/callback\"],
+  \"scope\": \"atproto transition:generic transition:chat.bsky\",
+  \"grant_types\": [\"authorization_code\", \"refresh_token\"],
+  \"response_types\": [\"code\"],
+  \"token_endpoint_auth_method\": \"none\",
+  \"application_type\": \"web\",
+  \"dpop_bound_access_tokens\": true
+}" > dist/oauth-client.json
