4fa72eaeb79e5e7f8546ad642cc2ec88db152667
ibro <sameoldlab@proton.me>
2025-05-01 00:09:55 +0100

    feat: account registration via bsky (#214)
    * add signup option for bsky.social
    
    * handle errors and loading state

--- src/lib/components/ServerBar.svelte
+++ src/lib/components/ServerBar.svelte
@@ -10,11 +10,14 @@
   import SidebarSpace from "$lib/components/SidebarSpace.svelte";
   import { Space } from "@roomy-chat/sdk";
   import { cleanHandle } from "$lib/utils.svelte";
+  import { atproto } from "$lib/atproto.svelte";
 
   const { spaces } = $props();
 
   let handleInput = $state("");
   let loginLoading = $state(false);
+  let signupLoading = $state(false);
+  const loadingAuth = $derived(signupLoading || loginLoading);
 
   let newSpaceName = $state("");
   let isNewSpaceDialogOpen = $state(false);
@@ -48,6 +51,16 @@
     }
 
     loginLoading = false;
+  }
+  async function signup() {
+    signupLoading = true;
+    try {
+      await atproto.oauth.signIn("https://bsky.social");
+    } catch (e: unknown) {
+      console.error(e);
+      loginError = e instanceof Error ? e.message.toString() : "Unknown error";
+    }
+    signupLoading = false;
   }
 </script>
 
@@ -138,7 +151,7 @@
             class="dz-input w-full"
           />
           <Button.Root
-            disabled={loginLoading || !handleInput}
+            disabled={loadingAuth || !handleInput}
             class="dz-btn dz-btn-primary"
           >
             {#if loginLoading}
@@ -147,6 +160,16 @@
             Log In with Bluesky
           </Button.Root>
         </form>
+        <Button.Root
+          onclick={signup}
+          disabled={loadingAuth}
+          class="dz-btn dz-btn-outline"
+        >
+          {#if signupLoading}
+            <span class="dz-loading dz-loading-spinner"></span>
+          {/if}
+          Signup with bsky.social
+        </Button.Root>
       {/if}
     </Dialog>
   </section>
