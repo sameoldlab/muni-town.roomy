3ea5134c8ea6fe3e38ab008dea4a6018db3c07f1
Zicklag <zicklag@katharostech.com>
2025-08-23 13:32:32 -0500

    feat: add a redirect to the spicylobster.itch.io space. (#385)

--- packages/app/src/hooks.ts
+++ packages/app/src/hooks.ts
@@ -3,10 +3,16 @@ import type { Reroute } from "@sveltejs/kit";
 export const reroute: Reroute = ({ url }) => {
   // FIXME: hack to make muni.town resolve for now
   const muniTown = "/muni.town";
+  const spicyLobster = "/spicylobster.itch.io";
   console.log("url", url.pathname);
   if (url.pathname.startsWith(muniTown)) {
     return (
       "/co_zgr1XyZyG3MHjswMoep5Na2eKr9" + url.pathname.slice(muniTown.length)
+    );
+  } else if (url.pathname.startsWith(spicyLobster)) {
+    return (
+      "/co_zGrLp9i59rcFeZCoW9KjULCbPkg" +
+      url.pathname.slice(spicyLobster.length)
     );
   }
 };
