6d6ab0c9718676a6abf1a4c9619e03d56a6d05ee
Zicklag <zicklag@katharostech.com>
2025-08-24 10:36:23 -0500

    feat: sort threads by recency in the all threads list. (#388)

--- packages/app/src/routes/(app)/[space=covalue]/index/+page.svelte
+++ packages/app/src/routes/(app)/[space=covalue]/index/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import {
     AllThreadsComponent,
+    co,
+    CoFeed,
+    getComponent,
     RoomyEntity,
     SubThreadsComponent,
+    ThreadComponent,
   } from "@roomy-chat/sdk";
   import { CoState } from "jazz-tools/svelte";
   import { ScrollArea } from "@fuxui/base";
@@ -10,6 +14,7 @@
   import MainLayout from "$lib/components/layout/MainLayout.svelte";
   import SidebarMain from "$lib/components/sidebars/SidebarMain.svelte";
   import { page } from "$app/state";
+  import { untrack } from "svelte";
 
   let space = $derived(
     new CoState(RoomyEntity, page.params.space, {
@@ -19,7 +24,7 @@
     }),
   );
 
-  let subthreads = $derived(
+  let subthreadEnts = $derived(
     new CoState(
       SubThreadsComponent,
       space?.current?.components?.[AllThreadsComponent.id],
@@ -36,6 +41,39 @@
       },
     ),
   );
+
+  function timelineTimestamp(t?: CoFeed<string>): number {
+    return (
+      Object.values(t?.perSession || {})
+        .map((x) => x.madeAt)
+        .sort((a, b) => b.getTime() - a.getTime())[0]
+        ?.getTime() || 0
+    );
+  }
+
+  let sortedSubthreads = $state.raw([]);
+  $effect(() => {
+    const subthreads = Object.values(subthreadEnts.current?.perAccount || {})
+      .map((x) => [...x.all].map((x) => x.value))
+      .flat()
+      .filter((x) => x && !x?.softDeleted) as co.loaded<typeof RoomyEntity>[];
+    untrack(async () => {
+      const resolved = await Promise.all(
+        subthreads.map(async (ent) => {
+          const thread = await getComponent(ent, ThreadComponent, {
+            resolve: { timeline: true },
+          });
+          const timestamp = timelineTimestamp(thread?.timeline as any);
+
+          return { ent, timestamp };
+        }),
+      );
+
+      sortedSubthreads = resolved
+        .sort((a, b) => b.timestamp - a.timestamp)
+        .map((x) => x.ent) as any;
+    });
+  });
 </script>
 
 <MainLayout>
@@ -48,12 +86,10 @@
   {/snippet}
 
   <ScrollArea class="h-full px-2 pb-4">
-    {#if subthreads.current}
+    {#if subthreadEnts.current}
       <ScrollArea class="h-full px-2 pb-4">
-        {#each Object.values(subthreads.current.perAccount)
-          .map((x) => [...x.all].map((x) => x.value))
-          .flat() as subthread}
-          {#if subthread && subthread.softDeleted == false}
+        {#each sortedSubthreads as subthread}
+          {#if subthread}
             <div class="mt-4">
               <BoardViewItem thread={subthread} />
             </div>
