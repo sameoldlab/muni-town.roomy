a4d8fbac96081ea7773bad938d36ca325fec5b6b
Zicklag <zicklag@katharostech.com>
2025-10-04 17:35:02 -0500

    docs: update the alpha warning to be more informative.

--- packages/app/src/lib/components/helper/EarlyAlphaWarning.svelte
+++ packages/app/src/lib/components/helper/EarlyAlphaWarning.svelte
@@ -1,31 +1,46 @@
 <script lang="ts">
-  import { Alert, Button } from "@fuxui/base";
-  import { onMount } from "svelte";
+  import { Alert } from "@fuxui/base";
+  // import { onMount } from "svelte";
 
-  let showAlphaWarning = $state(false);
-  onMount(() => {
-    showAlphaWarning = localStorage.getItem("showAlphaWarning") !== "false";
-  });
+  let showAlphaWarning = $state(true);
+  // onMount(() => {
+  //   showAlphaWarning = localStorage.getItem("showAlphaWarning") !== "false";
+  // });
 </script>
 
 {#if showAlphaWarning}
   <div class="p-2">
-    <Alert type="warning" class="text-sm max-w-2xl mx-auto bg-amber-100/30">
-      <span class="flex items-center gap-2 text-amber-800 dark:text-amber-200"
-        >Roomy is in an early "Alpha" state. This means that it is a
-        work-in-progress and is under heavy development. Many things may not
-        work as expected and data is preserved on a best-effort basis for the
-        time being.
-        <span class="flex flex-row-reverse grow">
-          <Button
+    <Alert type="warning" class=" max-w-2xl mx-auto bg-amber-100/30">
+      <div class="prose text-sm">
+        <p>
+          Roomy is in an unstable, early "Alpha" state. This means that it is a
+          work-in-progress and is under heavy development. Many things may not
+          work as expected and, though we do have backups as a precaution, <strong
+            class="text-white">data might not be preserved</strong
+          >.
+        </p>
+        <p>
+          If you would like to try Roomy as an early adopter reach out to us on <a
+            class="text-blue-400"
+            href="https://discord.gg/BkEpMzzh38">Discord</a
+          >
+          or
+          <a class="text-blue-400" href="https://bsky.app/profile/roomy.space"
+            >Bluesky</a
+          > and we can consider helping you specifically to maintain your data across
+          updates to Roomy, until it becomes stable.
+        </p>
+      </div>
+
+      <span class="flex flex-row-reverse grow">
+        <!-- <Button
             size="sm"
             variant="amber"
             onclick={() => {
               showAlphaWarning = false;
               localStorage.setItem("showAlphaWarning", "false");
             }}>Dismiss</Button
-          >
-        </span>
+          > -->
       </span>
     </Alert>
   </div>
