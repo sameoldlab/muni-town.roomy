45976ef3d6dea197b937993eb38d98d647c2d9eb
Zicklag <zicklag@katharostech.com>
2025-10-08 12:41:07 -0500

    chore: remove unused files from the SQLite POC.

--- packages/app/src/lib/workers/materializer.ts
+++ packages/app/src/lib/workers/materializer.ts
@@ -5,12 +5,11 @@ import type {
 } from "./backendWorker";
 import { _void, type CodecType } from "scale-ts";
 import { eventCodec, eventVariantCodec, GroupMember, id } from "./encoding";
-import schemaSql from "./db/schema.sql?raw";
+import schemaSql from "./schema.sql?raw";
 import { decodeTime } from "ulidx";
 import { sql } from "$lib/utils/sqlTemplate";
 import type { SqliteWorkerInterface } from "./types";
 import type { Agent } from "@atproto/api";
-import type { EdgesMap } from "./db/types/edges";
 import type { LeafClient } from "@muni-town/leaf-client";
 
 export type EventType = ReturnType<(typeof eventCodec)["dec"]>;
@@ -590,3 +589,51 @@ function edgePayload<EdgeLabel extends "reaction" | "member" | "ban">(
 ) {
   return JSON.stringify(payload);
 }
+
+export type EdgeLabel =
+  | "child"
+  | "parent"
+  | "subscribe"
+  | "member"
+  | "ban"
+  | "hide"
+  | "pin"
+  | "last_read"
+  | "embed"
+  | "reply"
+  | "link"
+  | "author"
+  | "reorder"
+  | "source"
+  | "avatar"
+  | "reaction";
+
+type EntityId = string;
+export interface EdgeReaction {
+  reaction: string;
+}
+
+export interface EdgeBan {
+  reason: string;
+  banned_by: EntityId;
+}
+
+export interface EdgeMember {
+  // delegation?: string;
+  can: "read" | "post" | "admin";
+}
+
+export type EdgesMap = {
+  [K in Exclude<EdgeLabel, "reaction" | "member" | "ban">]: null;
+} & {
+  reaction: EdgeReaction;
+  ban: EdgeBan;
+  member: EdgeMember;
+};
+
+/** Given a tuple of edge names, produces a record whose keys are exactly
+ * those edge names and whose values are arrays of the corresponding edge types.
+ */
+export type EdgesRecord<TRequired extends readonly EdgeLabel[]> = {
+  [K in TRequired[number]]: [EdgesMap[K], EntityId];
+};
