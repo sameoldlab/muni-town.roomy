093aecc65aa7c7d1e6d115f6689f6238218f2a6b
Zicklag <zicklag@katharostech.com>
2025-11-21 08:49:49 -0600

    fix: remove unneeded chat scopes and add file upload scopes.

--- packages/app/src/lib/workers/backendWorker.ts
+++ packages/app/src/lib/workers/backendWorker.ts
@@ -71,28 +71,35 @@ const atprotoOauthScope = [
   `atproto`, // Required just to login to atproto
 
   `rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app%23bsky_appview`, // Access to query the Bluesky profile
-  ...[
-    "chat.bsky.actor.deleteAccount",
-    "chat.bsky.actor.exportAccountData",
-    "chat.bsky.convo.acceptConvo",
-    "chat.bsky.convo.deleteMessageForSelf",
-    "chat.bsky.convo.getConvoAvailability",
-    "chat.bsky.convo.getConvoForMembers",
-    "chat.bsky.convo.getConvo",
-    "chat.bsky.convo.getLog",
-    "chat.bsky.convo.leaveConvo",
-    "chat.bsky.convo.listConvos",
-    "chat.bsky.convo.muteConvo",
-    "chat.bsky.convo.removeReaction",
-    "chat.bsky.convo.sendMessageBatch",
-    "chat.bsky.convo.unmuteConvo",
-    "chat.bsky.convo.addReaction",
-    "chat.bsky.convo.updateAllRead",
-    "chat.bsky.convo.updateRead",
-    "chat.bsky.moderation.getActorMetadata",
-    "chat.bsky.moderation.getMessageContext",
-    "chat.bsky.moderation.updateActorAccess",
-  ].map((lxm) => `rpc:${lxm}?aud=did:web:api.bsky.chat%23bsky_chat`),
+
+  // These are the scopes needed for accessing Bluesky DMs, but since we don't have the UI for that
+  // yet, lets not ask for permission to it.
+
+  // ...[
+  //   "chat.bsky.actor.deleteAccount",
+  //   "chat.bsky.actor.exportAccountData",
+  //   "chat.bsky.convo.acceptConvo",
+  //   "chat.bsky.convo.deleteMessageForSelf",
+  //   "chat.bsky.convo.getConvoAvailability",
+  //   "chat.bsky.convo.getConvoForMembers",
+  //   "chat.bsky.convo.getConvo",
+  //   "chat.bsky.convo.getLog",
+  //   "chat.bsky.convo.leaveConvo",
+  //   "chat.bsky.convo.listConvos",
+  //   "chat.bsky.convo.muteConvo",
+  //   "chat.bsky.convo.removeReaction",
+  //   "chat.bsky.convo.sendMessageBatch",
+  //   "chat.bsky.convo.unmuteConvo",
+  //   "chat.bsky.convo.addReaction",
+  //   "chat.bsky.convo.updateAllRead",
+  //   "chat.bsky.convo.updateRead",
+  //   "chat.bsky.moderation.getActorMetadata",
+  //   "chat.bsky.moderation.getMessageContext",
+  //   "chat.bsky.moderation.updateActorAccess",
+  // ].map((lxm) => `rpc:${lxm}?aud=did:web:api.bsky.chat%23bsky_chat`),
+
+  "blob:*/*", // Allow all blob uploads
+  "repo:space.roomy.upload", // And creating roomy upload records
 
   `repo:${CONFIG.streamNsid}`, // Access to the stream collection
   `repo:${CONFIG.streamHandleNsid}`, // Access to the stream handle collection
