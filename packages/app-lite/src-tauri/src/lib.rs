b72d6ec718dfc8d8eaea6996d6264d2e86d6dbe6
ibro <sameoldlab@proton.me>
2026-08-05 11:02:48 +0100

    add tauri plugin: notifications

--- packages/app-lite/src-tauri/src/lib.rs
+++ packages/app-lite/src-tauri/src/lib.rs
@@ -18,6 +18,7 @@ pub fn run() {
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_notification::init())
         .setup(|app| {
             // runtime deep_link registration
             #[cfg(any(target_os = "linux", windows))]
